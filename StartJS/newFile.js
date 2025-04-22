75e4891496c96732be04bb97e0d355634dbee9ee
yashasp25 <yashasp2k3@gmail.com>
2025-04-22 13:09:22 +0530

    today

---
+++ StartJS/newFile.js
@@ -0,0 +1,18 @@
+var age =18
+let gender = "Male"
+const institute = 'x-workz'
+document.writeln(age)
+
+let em1={id:12, employeeName:'qs', salary:100000}
+
+let em2= new Object();
+em2.id=13;
+em2=employeeName="Ravan";
+em2.salary=20000;
+em2.id=14;
+
+function em3(id, employeeName, salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary= salary;
+}
